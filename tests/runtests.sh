42a748ad12bd172c17e9cce519eaa21da2d88244
Morgan Phillips <winter2718@gmail.com>
2015-12-27 15:05:03 -0600

    Give test-runner appropriate exit codes

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -10,11 +10,19 @@ if [ $# = 1 ]; then
     tests=${@:2};
 fi
 
+failure=false;
 for t in $tests; do
     $1 $t > /dev/null;
     if [ $? = 0 ]; then
         echo "$t : passed";
     else
         echo "$t : failed";
+        failure=true;
     fi
 done
+
+if [ $failure = true ]; then
+    exit 1;
+else
+    exit 0;
+fi
